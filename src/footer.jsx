33232238e5abb7b8929428052f1fbe8eaaabd6f1
sifeddineEddr <sifeddine.edr@gmail.com>
2023-01-29 19:47:58 +0100

    deploy

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
       <p className="font-bold text-3xl text-white">TRAVELLA</p>
       <div className="flex w-2/5 justify-around md:w-1/3 md:py-1">
         <a href="http://www.facebook.com" target="_blank" rel="noreferrer">
-          <img className="w-2/3" src="/images/fb-icon.svg" alt="fb-icon" />
+          <img className="w-2/3" src="images/fb-icon.svg" alt="fb-icon" />
         </a>
         <a href="http://www.instagram.com" target="_blank" rel="noreferrer">
-          <img className="w-2/3" src="/images/ig-icon.svg" alt="ig-icon" />
+          <img className="w-2/3" src="images/ig-icon.svg" alt="ig-icon" />
         </a>
         <a href="http://www.twitter.com" target="_blank" rel="noreferrer">
           <img className="w-2/3" src="/images/tw-icon.svg" alt="tw-icon" />
